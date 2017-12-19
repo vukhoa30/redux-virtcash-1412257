e82a21a0bd247bc57be2442081317ff0edb57c3c
vukhoa30 <vukhoa32@gmail.com>
2017-12-19 12:39:28 +0700

    fix a bit more

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SignUpForm from '../SignUpForm/SignUpForm'
 
 import './SignUp.css';
 import '../SignIn/SignIn.css'
 
-export default class SignUp extends LinkedComponent {
+export default class SignUp extends Component {
   constructor(props) {
     super(props);
   }
